bdd424ff45c72d133c711b6922cf7dbf1dd60197
JaimeMrtinzMndoz <minijaime87@gmail.com>
2014-03-14 12:57:52 +0100

    Aplicacion
    Facebook

---
+++ MyApp/MyApp/FacebookDemo.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Facebook;
+
+namespace MyApp
+{
+    public partial class FacebookDemo : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var fb = new FacebookClient();
+             if (Request.QueryString["code"] != null)
+            {
+                string accessCode = Request.QueryString["code"].ToString();
+ 
+                
+ 
+                // throws OAuthException 
+                dynamic result = fb.Post("oauth/access_token", new
+                {
+ 
+                    client_id = "your_app_id",
+ 
+                    client_secret = "your_app_secret",
+ 
+                    redirect_uri = "http://localhost:8779/FacebookDemo.aspx",
+ 
+                    code = accessCode
+ 
+                });
+ 
+                var accessToken = result.access_token;
+              }
+        }
+
+            protected void Button1_Click(object sender, EventArgs e)
+       {
+           var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+               {
+ 
+                   client_id = "your_app_id",
+ 
+                   redirect_uri = "http://localhost:8779/FacebookDemo.aspx",
+ 
+                   response_type = "code",
+ 
+                   scope = "email" // Add other permissions as needed
+ 
+               });
+               Response.Redirect(loginUrl.AbsoluteUri);
+        }
+        
+    }
+}
